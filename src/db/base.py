0e1c7b49f50424a13b3d2773cafa08f09740e21a
fersus <deriabin_85@mail.ru>
2023-08-11 19:57:39 +0300

    Add Alembic config

---
+++ src/db/base.py
@@ -0,0 +1,1 @@
+from src.db.base_class import Base
--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,6 @@ class Settings:
     POSTGRES_USER: str = os.getenv("POSTGRES_USER")
     POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
     POSTGRES_SERVER: str = os.getenv("POSTGRES_SERVER", "localhost")
-    # default postgres port is 5432
     POSTGRES_PORT: str = os.getenv("POSTGRES_PORT", 5432)
     POSTGRES_DB: str = os.getenv("POSTGRES_DB", "tdd")
     DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
